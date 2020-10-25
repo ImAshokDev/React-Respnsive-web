f984c4371389cfa7477313825838fa83341f1fb4
Ashok <imashokdev@gmail.com>
2020-10-25 18:39:35 +0530

    done with signup button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaTimes, FaBars } from "react-icons/fa";
 import { IconContext } from "react-icons/lib";
+import { Button } from "../../globalStyles";
 import {
   Nav,
   NavbarContainer,
@@ -10,12 +11,30 @@ import {
   NavMenu,
   NavItem,
   NavLink,
+  NavItemBtn,
+  NavBtnLink,
 } from "./NavbarStyle";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
   return (
     <>
       <IconContext.Provider value={{ color: "#fff" }}>
@@ -39,6 +58,20 @@ const Navbar = () => {
               <NavItem>
                 <NavLink to="/">Products</NavLink>
               </NavItem>
+
+              <NavItemBtn>
+                {button ? (
+                  <NavBtnLink to="/sign-up">
+                    <Button primary>SIGN UP</Button>
+                  </NavBtnLink>
+                ) : (
+                  <NavBtnLink to="/sign-up">
+                    <Button fontBig primary>
+                      SIGN UP
+                    </Button>
+                  </NavBtnLink>
+                )}
+              </NavItemBtn>
             </NavMenu>
           </NavbarContainer>
         </Nav>
