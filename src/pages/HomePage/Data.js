fc8eba5d25171034c32153af9443cadcbbd31759
Ashok <imashokdev@gmail.com>
2020-10-26 18:28:58 +0530

    done with page

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -1,4 +1,7 @@
 import svg1 from "../../images/svg-1.svg";
+import svg2 from "../../images/svg-2.svg";
+import svg3 from "../../images/svg-3.svg";
+import profile from "../../images/profile.jpg";
 
 export const homeObjOne = {
   lightBg: false,
@@ -15,4 +18,53 @@ export const homeObjOne = {
   img: svg1,
   alt: "Image",
   start: "",
+};
+
+export const homeObjTwo = {
+  lightBg: true,
+  primary: false,
+  imgStart: "start",
+  lightTopLine: true,
+  topLine: "Mergeting Agency",
+  lightText: false,
+  headline: "Lead Generation Specialist for online Businesses",
+  lightTextDesc: false,
+  description:
+    "We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals",
+  buttonLabel: "Get Started",
+  img: profile,
+  alt: "Image",
+  start: "true",
+};
+export const homeObjThree = {
+  lightBg: false,
+  primary: true,
+  imgStart: "",
+  lightTopLine: true,
+  topLine: "Mergeting Agency",
+  lightText: true,
+  headline: "Lead Generation Specialist for online Businesses",
+  lightTextDesc: true,
+  description:
+    "We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals",
+  buttonLabel: "Get Started",
+  img: svg2,
+  alt: "Image",
+  start: "",
+};
+export const homeObjFour = {
+  lightBg: false,
+  primary: true,
+  imgStart: "start",
+  lightTopLine: true,
+  topLine: "Mergeting Agency",
+  lightText: true,
+  headline: "Lead Generation Specialist for online Businesses",
+  lightTextDesc: true,
+  description:
+    "We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals",
+  buttonLabel: "Get Started",
+  img: svg3,
+  alt: "Image",
+  start: "start",
 };
